3b298bc631bed3d79bfe4f5b489db48f88293314
stordahl <jacob.stordahl@stylitics.com>
2022-11-02 02:18:57 -0500

    move posts into folder

--- packages/site/src/routes/writing/[slug]/+page.ts
+++ packages/site/src/routes/writing/[slug]/+page.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load:PageLoad = async ({ params }) => {
-  const post = await import(`../${params.slug}.md`)
+  const post = await import(`../posts/${params.slug}.md`)
   const { title, date, status } = post.metadata
   const content = post.default
 
